505f2063653f53fdad473bee7ff77fceec68be0d
VictorDalence <victor.dalence@ucb.edu.bo>
2026-03-04 12:47:15 -0400

    Add files via upload

---
+++ PRACTICA_03/Ejercicio_03_01.cpp
@@ -0,0 +1,8 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Victor Guillermo Dalence Deheza
+// Carnet: 12420464
+// Fecha creación: 28/02/2026
+
+#include<iostream>
+#include<time.h>
+#include<random>
